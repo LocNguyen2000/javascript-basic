1f3511d06527f44d07990f431c4caa1ab1b96f9f
Loc Nguyen <lolkun2000@gmail.com>
2023-04-18 13:34:18 +0700

    add resource

---
+++ Homework/HW2-async/async-02-resource/js/main.js
@@ -0,0 +1,22 @@
+(function () {
+  var app = angular.module("myApp", []);
+
+  app.controller("demoCtrl", ["$scope", DemoController]);
+
+  // // Problem 1: Callbacks
+  // function DemoController($scope) {
+  //    TODO: Code here!
+  // }
+
+  // Problem 2: Promise
+  // function DemoController($scope) {
+  //    TODO: Code here!
+  // }
+
+  // Problem 03: Generators // BO QUA
+
+  // Problem 04: async/await
+  // async function DemoController($scope) {
+  //   TODO: Code here!
+  // }
+})();
